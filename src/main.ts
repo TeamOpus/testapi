e995b37cbb4054a926070c1083dff9ed5c6b162b
TeamOpus <srvopus@gmail.com>
2025-08-14 00:57:35 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import defaultRoute from "./routes/default";
 
 const app: Application = express();
 const port: number = parseInt(process.env.PORT) || 4131;
-const host: string = process.env.HOST || "localhost"; // Add this line
+const host: string = process.env.HOST || "69.62.84.40"; // Your custom IP
 
 app.set("views", "views");
 app.set("view engine", "pug");
@@ -11,7 +11,7 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use(defaultRoute);
 
-// Modified listen call with both host and port
+// Update the listen call and console.log
 app.listen(port, host, () => {
     console.log(`App started on ${host}:${port}`);
 });
