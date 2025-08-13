c1362ba2874ab1ff7ba4983a337928c4d9b9e126
TeamOpus <srvopus@gmail.com>
2025-08-14 00:56:49 +0530

    Update default.ts

--- src/routes/default.ts
+++ src/routes/default.ts
@@ -1,17 +1,29 @@
-import express, { Application } from "express";
-import defaultRoute from "./routes/default.ts";
+import {Request, Response, Router} from "express";
+import {VideoData} from "../types/youtube";
+import {YouTube} from "../services/youtube";
 
-const app: Application = express();
-const port: number = parseInt(process.env.PORT) || 4131;
-const host: string = process.env.HOST || "69.62.84.40"; // Your custom IP
+const router: Router = Router();
 
-app.set("views", "views");
-app.set("view engine", "pug");
+router.get("/", async (req: Request, res: Response) => {
 
-app.use(express.static("public"));
-app.use(defaultRoute);
+    // No video searched -> startpage
+    if(!req.query.url) {
+        res.render("index");
+        return;
+    }
 
-// Update the listen call and console.log
-app.listen(port, host, () => {
-    console.log(`App started on ${host}:${port}`);
+    let video: VideoData = await YouTube.getVideoInfo(req.query.url);
+
+    // Cant get video -> error page
+    if(!video) {
+        res.render("error");
+        return;
+    }
+
+    res.render("video", video);
+
 });
+
+router.get("/about", (req, res) => res.render("about"));
+
+export default router;
